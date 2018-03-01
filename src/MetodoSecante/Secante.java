3bfd2016f1e1a6bfa7bfdbbde1656e9552a8e271
Carlos <Carlos@LAPTOP-0P09H231>
2018-03-01 14:09:49 -0600

    metodos

---
+++ src/MetodoSecante/Secante.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MetodoSecante;
+
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Secante {
+     double Xi1, Xi;
+    double fxi_1, fXi, fXi_1_fxi, fXi_fXi_1_xi, Raiz, Error;
+    double e = 2.718281828;
+    int n;
+
+    public double getXi() {
+        return Xi;
+    }
+
+    public void setXi(double Xi) {
+        this.Xi = Xi;
+    }
+
+    public double getXi1() {
+        return Xi1;
+    }
+
+    public void setXi1(double Xi1) {
+        this.Xi1 = Xi1;
+    }
+
+    public int[][] Operacion(JTable x, int n) {
+        int arreglo[][] = new int[n][n];
+        String cabecera[] = {"i", "Xi-i", "Xi", "f(xi-1)", "f(Xi)", "f(Xi-1)-f(xi)", "f(Xi)*f(Xi-1-xi)", "Raix", "Error"};
+
+        DefaultTableModel T = new DefaultTableModel(null, cabecera);
+        for (int i = 0; i < n + 1; i++) {
+            String Fila[] = {"" + i, "" + Xi1, "" + Xi, "" + fxi_1, "" + fXi, "" + fXi_1_fxi, "" + fXi_fXi_1_xi, "" + Raiz, "" + Error};
+            if (i == 0) {
+
+                fxi_1 = Math.pow(e, -getXi1()) - getXi1();
+                fXi = Math.pow(e, -getXi()) - getXi();
+                fXi_1_fxi = fxi_1 - fXi;
+                fXi_fXi_1_xi = (fXi * (fxi_1 - getXi()));
+                Raiz = ((fXi_fXi_1_xi / fXi_1_fxi) + getXi());
+                Error = -1;
+            } else {
+                if (i == 1 || i != 1) {
+                    Xi1 = Xi;
+                    Xi = fxi_1;
+                    fxi_1 = Math.pow(e, -Xi1) - Xi1;
+                    fXi = Math.pow(e, -Xi) - Xi;
+                    fXi_1_fxi = fxi_1 - fXi;
+                    fXi_fXi_1_xi = (fXi * (fxi_1 - Xi));
+                    Raiz = ((fXi_fXi_1_xi / fXi_1_fxi) + Xi);
+                    Error = 0;
+                }
+                T.addRow(Fila);
+            }
+            x.setModel(T);
+        }
+
+        return arreglo;
+    }
+}
+
--- src/metodo_secante/Secante.java
+++ src/metodo_secante/Secante.java
@@ -42,7 +42,6 @@ public class Secante extends javax.swing.JFrame {
         N_iteracion = new javax.swing.JTextField();
         jLabel5 = new javax.swing.JLabel();
         jSeparator1 = new javax.swing.JSeparator();
-        jLabel4 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -84,8 +83,6 @@ public class Secante extends javax.swing.JFrame {
         jLabel5.setBackground(new java.awt.Color(255, 255, 204));
         jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagen/Captura.PNG"))); // NOI18N
 
-        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Metodo de la secante.PNG"))); // NOI18N
-
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -109,18 +106,14 @@ public class Secante extends javax.swing.JFrame {
                         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(jLabel5)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 357, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(50, 50, 50))))
+                        .addGap(50, 413, Short.MAX_VALUE))))
             .addComponent(jSeparator1)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
@@ -237,7 +230,6 @@ public class Secante extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
